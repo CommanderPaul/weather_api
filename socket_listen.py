e2a51745f0a3690ca710ee8f5eb02668cca7ee48
commanderPaul <gaussandgo@gmail.com>
2019-03-07 14:06:16 -0800

    Initial commit

---
+++ socket_listen.py
@@ -0,0 +1,22 @@
+#!/bin/env python3.6
+
+import socket
+
+s = socket.socket()
+
+# is hostname defined?
+#host = socket.gethostname()
+host = "192.168.33.10"      # works, but should use hostname
+port = 80
+s.bind((host,port))
+s.listen(5)
+while True:
+
+    # connection and addess are returned
+    c, addr = s.accept()
+    print("Connection accepted from " + repr(addr[1]))
+
+    # connection.send() needs what?
+    c.send("Server approved connection\n".encode('utf-8'))
+    print (repr(addr[1]) + ": " + c.recv(1026).decode())
+    c.close()
